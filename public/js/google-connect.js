bd2378f4308770c013e1b6a446cb1efdffed0adf
greedjow <eric.diamonika@hetic.net>
2018-06-13 23:41:41 +0200

    add google connect

--- public/js/google-connect.js
+++ public/js/google-connect.js
@@ -1,7 +1,7 @@
 googleSignIn=()=>{
 
     base_provider = new firebase.auth.GoogleAuthProvider()
-    firebase.auth().signInWithRedirect(base_provider).then(function(result){
+    firebase.auth().signInWithPopup(base_provider).then(function(result){
         console.log(result)
         console.log("Success..Google Account Linked")
     }).catch((function(err){
